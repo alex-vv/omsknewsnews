94d142c8dcde4eecc10a7f3e87ef68a7ced0a3ad
Alexander Vykhodtsev <a.a.vykhodtsev@gmail.com>
2026-09-25 16:59:06 +0600

    Fix SSL p2

--- web/lib/db.js
+++ web/lib/db.js
@@ -1,10 +1,12 @@
+import { parse } from "pg-connection-string";
 import { Pool } from "pg";
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
+const ca = process.env.DATABASE_CA_CERT;
+const config = parse(process.env.DATABASE_URL);
+config.ssl = ca
+  ? { ca: ca.replace(/\\n/g, "\n"), rejectUnauthorized: true }
+  : { rejectUnauthorized: false };
+
+const pool = new Pool(config);
 
 export default pool;
