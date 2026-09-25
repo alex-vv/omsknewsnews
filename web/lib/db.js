4aef79a568506bd3e28031fceec319f7cb9f6066
Alexander Vykhodtsev <a.a.vykhodtsev@gmail.com>
2026-09-25 22:50:05 +0600

    Allow local non-ssl db connections

--- web/lib/db.js
+++ web/lib/db.js
@@ -3,9 +3,14 @@ import { Pool } from "pg";
 
 const ca = process.env.DATABASE_CA_CERT;
 const config = parse(process.env.DATABASE_URL);
-config.ssl = ca
-  ? { ca: ca.replace(/\\n/g, "\n"), rejectUnauthorized: true }
-  : { rejectUnauthorized: false };
+
+if (ca) {
+  config.ssl = { ca: ca.replace(/\\n/g, "\n"), rejectUnauthorized: true };
+} else if (config.ssl === undefined) {
+  config.ssl = false;
+} else if (config.ssl !== false) {
+  config.ssl = { rejectUnauthorized: false };
+}
 
 const pool = new Pool(config);
 
